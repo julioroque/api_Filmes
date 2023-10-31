6e91573093c969272985269a69940ee155363d64
julioroquesilva <116984649+julioroque@users.noreply.github.com>
2023-10-31 20:33:15 -0300

    MovieCard e Navbar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import {FaStar} from "react-icons/fa"
 
 const imageUrl = import.meta.env.VITE_IMG;
 
-const MovieCard = (movie, showLink = true) => {
+const MovieCard = ({movie, showLink = true}) => {
   return (
     <div className="movie-card">
         <img src={imageUrl + movie.poster_path} alt={movie.title} />
@@ -12,7 +12,8 @@ const MovieCard = (movie, showLink = true) => {
         <p>
           <FaStar /> {movie.vote_average}
         </p>
-        {showLink && <Link to={``}
+        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+      
     </div>
   )
 }
